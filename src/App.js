d3266239bdfc824fe30b0788369990659f23e3da
hayatkhan1196 <hayatkhan1196@gmail.com>
2021-07-19 18:21:48 +0500

    updates in counter app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import "./App.css";
 import { connect } from "react-redux";
 
 class App extends Component {
+
   render() {
+    const { age, onAgeUp, onAgeDown } = this.props
     return (
+
       <div className="App">
         <div className="Age-label">
-          your age: <span>{this.props.age}</span>
+          your age: <span>{age}</span>
         </div>
-        <button onClick={this.props.onAgeUp}>Age UP</button>
-        <button onClick={this.props.onAgeDown}>Age Down</button>
+        <button onClick={onAgeUp}>Age UP</button>
+        <button onClick={onAgeDown}>Age Down</button>
       </div>
     );
   }
